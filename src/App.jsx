8ca7519d16280d8f2fe92e085a72eaf6ddf6ae14
zaynabxon412 <bohodirovazaynabxon@gmail.com>
2026-06-20 10:08:13 +0500

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './Components/Hero/Hero'
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
    <div>
     <h1>figma__team</h1>
+    <Hero />
    </div>
   )
 }
